3c89a069a48c22714e7f7779d62cbde01e044105
Behar Hyseni <behar.hyseni@alumni.ubc.ca>
2018-09-30 14:41:40 -0700

    extracts routers nodes, their links, costs and capacities

--- shortest_path.py
+++ shortest_path.py
@@ -5,6 +5,7 @@ import json as json
 # Do not forget to change to your path 
 FILE = 'C:/Users/behar/Desktop/Shortest_Path_NANOG/topology.json'
 
+# Open the JSON file that contains the router information:
 with open(FILE) as topology:    data = json.load(topology)
 
 # Constains the JSON list of "ted-database-information"
@@ -19,10 +20,50 @@ dict_length = len(ted_database)
 # Contains the names of the Node IDs
 id_list = []
 
-
-for i in range(0, dict_length-1):
-    id = ted_database[i]["ted-database-id"][0]["data"]
+for i in range(0, dict_length):
+    id = ted_database[i]["ted-database-id"][0]["data"].encode("ascii")
     id_list.append(id)
     
-
+print "Nodes: "
 print id_list
+print " "
+
+ted_link = ted_database[0]["ted-link"]
+links = []
+cost = []
+capacity = []
+
+
+for n in range(0,len(ted_database)):
+   
+    list = []
+    cost_list = []
+    capacity_list = []
+    for i in range(0,len(ted_database[n]["ted-link"])):
+        list.append(ted_database[n]["ted-link"][i]["ted-link-to"][0]["data"].encode("ascii"))
+        cost_list.append(ted_database[n]["ted-link"][i]["ted-link-metric"][0]["data"].encode("ascii"))
+        capacity_list.append(ted_database[n]["ted-link"][i]["ted-link-static-bandwidth"][0]["data"].encode("ascii"))
+    links.append(list)
+    cost.append(cost_list)
+    capacity.append(capacity_list)
+    
+
+print "links: "
+print links
+print " "
+
+print "links costs: "
+print cost
+print " "
+
+print "links capacities: "
+print capacity
+print " "
+
+
+
+
+
+
+
+#print id_list
